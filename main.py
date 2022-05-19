dd744d2d6721c063047909160427a5b2c3033c95
Valentin <79686793+KubanovValentin@users.noreply.github.com>
2022-05-19 15:13:40 +0000

    1

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ SPORTS = [
 @app.route("/")
 def index():
 
-    return render_template("index.html")
+    return render_template("index.html",sports=SPORTS)
 
 
-@app.route("/register", method=["POST"])
+@app.route("/register", methods=["POST"])
 def register():
-    if not request.form.get("name") or not request.form.get("sport"):
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
       return render_template("failure.html")
     return render_template("success.html")
 
